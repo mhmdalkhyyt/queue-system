de0844f1108cdd27f6d99311e435124fb2ebd467
Eric Pettersson <[eric.k.r.pettersson@gmail.com]>
2022-09-05 14:06:53 +0200

    Added virtual environment for python

--- Server/testClient1.py
+++ Server/testClient1.py
@@ -1,10 +1,20 @@
 import zmq
 
 context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.connect('tcp://127.0.0.1:7000')
-socket.setsockopt_string(zmq.SUBSCRIBE, '')
+socket = context.socket(zmq.DEALER)
+socket.connect('tcp://http://tinyqueue.cognitionreversed.com/:5556')
+# socket.setsockopt_string(zmq.SUBSCRIBE, '')
 
-while(True):
-    message = socket.recv_pyobj()
-    print (message)
+thisName = {
+    "enterQueue": "true",
+    "name": "<Testnamn>"
+}
+
+
+def send_name():
+    socket.send_json(thisName)
+
+#
+# while (True):
+#     message = socket.recv_pyobj()
+#     print(message)
