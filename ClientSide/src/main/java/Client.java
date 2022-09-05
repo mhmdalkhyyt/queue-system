37874d7053dc784d7421782486547a78015c6c20
mhmdalkhyyt <86086618+mhmdalkhyyt@users.noreply.github.com>
2022-09-05 16:06:50 +0200

    Initial Client
    Initial clinet implementation, Hashtable returned from tinyqueue

--- ClientSide/src/main/java/Client.java
+++ ClientSide/src/main/java/Client.java
@@ -1,10 +1,25 @@
+import org.zeromq.*;
+import org.zeromq.ZMQ.Socket;
 
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hellooo");
+		if(args.length > 0) {
+			try(ZContext context = new ZContext()){
+				Socket socket = context.createSocket(SocketType.DEALER);
+				socket.connect(args[0]);
+				String msg = "{\"subscribe\":true}";
+				socket.send(msg);
+				
+				byte[] response = socket.recv();
+				
+				System.out.println("Response from server: " + new String(response, ZMQ.CHARSET)); 
+				
+			}
+		}
+		else {
+			System.out.println("Please specify URL");
+		}
 
 	}
 
