a568752cd2cbcf5aeda45dfb958925597085839d
a20muhal <102594578+a20muhal@users.noreply.github.com>
2022-09-12 14:40:15 +0200

    Client working heartbeat

--- DSAssign1/src/main/java/com/dsassign1/DSAssign1.java
+++ DSAssign1/src/main/java/com/dsassign1/DSAssign1.java
@@ -5,8 +5,6 @@
 
 package com.dsassign1;
 
-
-
 /**
  *
  * @author muham
@@ -17,8 +15,14 @@ import org.zeromq.*;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ.Socket;
 
-import com.fasterxml.jackson.core;
+
+//import com.fasterxml.jackson.core;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class DSAssign1
@@ -33,32 +37,44 @@ public class DSAssign1
             try (ZContext context = new ZContext()) {
                 Socket socket = context.createSocket(SocketType.DEALER);
                 socket.connect(args[0]);
-                
-                
-                String name = "Mometo";
+
                 ObjectMapper mapper = new ObjectMapper();
                 
-               
-                
                 String payload = "{\"enterQueue\": true,\n" + " \"name\": \"" + student.getName() + "\"}";
-                
-                
-                
+
                 socket.send(payload);
                 
                 byte[] response = socket.recv();
                 System.out.println("Reply from server: " + new String(response, ZMQ.CHARSET));
-                
+               
 
-                //Set into ListObj
-                
-                Map<String, Object> studentMap = mapper.readValue(new String(response, ZMQ.CHARSET)), new TypeReference<Map<String,Object>>(){});
-                
+                while(true){
+                    socket.send("{}");
+                    Thread.sleep(4000);
+                    System.out.println("4 secs passed, sending heartbeat");
+
+                }
             }
+
         }
         else{
             System.out.println("Please specify URL");
         
         }
     }
-}
+
+    public static void sendHeartbeat(Socket socket){
+        System.out.println("Trying to send heartbeat");
+        Runnable heartbeat = () -> {
+
+                String h = "{}";
+                socket.send(h);
+
+                System.out.println("helloo heartbeat");
+
+
+        };
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        executorService.scheduleAtFixedRate(heartbeat, 0, 3, TimeUnit.SECONDS);
+        }
+    }
