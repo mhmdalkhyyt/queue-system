fdd05518d2edcdd8d9a77071d81a6ba8903c47b7
Eric Pettersson <[eric.k.r.pettersson@gmail.com]>
2022-09-19 15:58:48 +0200

    Server kinda works, needs better parsing

--- Server/server.py
+++ Server/server.py
@@ -2,6 +2,7 @@ import tkinter
 import zmq
 import os
 import binascii
+import json
 from tkinter import *
 import threading
 
@@ -19,12 +20,15 @@ class GUI(threading.Thread):
         os._exit(0)
 
     def update_queue(self, d):
-        i=1
+        i = 1
         if self.queue_box is not None:
             self.queue_box.delete(1.0, END)
 
             for x in d:
-                self.queue_box.insert(END, str(i) + ': ' + x + '\n')
+                sliced_string = x[1]
+                sliced_json=json.loads(sliced_string)
+                #sliced_string = sliced_string[30:-2]
+                self.queue_box.insert(END, str(i) + ': ' + sliced_json['name'] + '\n')
                 i = i + 1
 
     def queue_button(self):
@@ -35,34 +39,39 @@ class GUI(threading.Thread):
         self.root.protocol("WM_DELETE_WINDOW", self.callback)
         self.root.title("Queue server")
         self.root.geometry('500x600')
-
-        # self.send_button = tkinter.Button(self.root, text='Remove next in queue', font=('Arial', 18),
-        # command=self.queue_button)
-        # self.send_button.pack(padx=20, pady=20)
-
         self.queue_box = tkinter.Text(self.root, font=('Arial', 16))
         self.queue_box.pack(padx=20)
 
         self.root.mainloop()
 
 
-
 context = zmq.Context()
-
+backend_socket = context.socket(zmq.ROUTER)
 # ------------------------------------------
 # Select the correct line for online or local communication
 # ------------------------------------------
 # socket.connect('tcp://tinyqueue.cognitionreversed.com:5556')
-socket = context.socket(zmq.ROUTER)
-socket.bind('tcp://127.0.0.1:7000')
+backend_socket.bind('tcp://127.0.0.1:7000')
+
+
+def send_service(mesg):
+    backend_socket.send_multipart(mesg)
+
+
+def string_creator(mesg):
+    concMessage = '1wd'
+    return concMessage
+
+
 # ------------------------------------------
 gui = GUI()
 subscribers = list()
 help_queue = list()
 
 while True:
-    msg = socket.recv_multipart()
+    msg = backend_socket.recv_multipart()
     msg_temp = msg[0]
+    ID = msg[0]
     msg_temp = binascii.hexlify(msg_temp).decode('ascii')
     msg[0] = msg_temp
 
@@ -71,26 +80,28 @@ while True:
 
             print('already in subs')
         else:
+            user = msg[1].decode('ascii')
             subscribers.append(msg[0])
             print(msg[0] + ' added to subs')
-            for x in range(len(subscribers)):
-                print(subscribers[x])
+            print(subscribers)
             print('................')
 
     elif 'enterQueue' in msg[1].decode('ascii'):
         if msg[0] in help_queue:
             print('already in queue')
         else:
+            user = msg[1].decode('ascii')
             print(msg[0] + ' added to queue')
-            help_queue.append(msg[0])
+            help_queue.append([msg[0], user])
             message = msg[1].decode('ascii')
             print(message)
             for x in range(len(help_queue)):
+                print('Queue printout:')
                 print(help_queue[x])
             print('................')
             gui.update_queue(help_queue)
-            temp = '{\'queue\': [{\'name\': \'zsczs\', \'ticket\': 6}, {\'name\': \'test\', \'ticket\': 8}, {\'name\': \'Guasg\', \'ticket\': 9}, {\'name\': \'Gaston\nGistab\', \'ticket\': 10}'
-            socket.send_json(temp)
+            temp = '{\"queue\": [{\"name\": \"rrrrtttfff\", \"ticket\": 1}], \"supervisors\": [], \"serverId\": \"server5530\"}'
+            send_service([ID, bytes(temp, 'UTF-8')])
 
     # print(type(msg[0]))
     # print(type(msg[1]))
