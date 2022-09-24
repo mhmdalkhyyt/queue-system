aebc3249343cfe22d160f913a028c645744efe73
a20muhal <102594578+a20muhal@users.noreply.github.com>
2022-09-24 18:11:50 +0200

    new gui

--- DSAssign1/src/main/java/com/dsassign1/DSAssign1.java
+++ DSAssign1/src/main/java/com/dsassign1/DSAssign1.java
@@ -14,11 +14,10 @@ import org.zeromq.ZMQ;
 import org.zeromq.*;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ.Socket;
+import org.json.*;
 
-
-//import com.fasterxml.jackson.core;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import javax.swing.text.html.HTMLDocument;
+import java.util.ArrayList;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -27,13 +26,14 @@ import java.util.concurrent.TimeUnit;
 
 public class DSAssign1
 {
+    static ArrayList<String> studentList = new ArrayList<>();
+    static JSONArray jsArr;
+    static GUI gui = new GUI();
+
     public static void main(String[] args) throws Exception
     {
-       initGUI();
-        GUI gui = new GUI();
 
-        //Person student = new Person("0", gui.getTextfield().getText());
-        System.out.println("Name for queue: " + gui.getStudentName().getName());
+        gui.init();
 
         Thread.sleep(10000);
 
@@ -42,12 +42,27 @@ public class DSAssign1
                     Socket socket = context.createSocket(SocketType.DEALER);
                     socket.connect(args[0]);
 
-                    String payload = "{\"enterQueue\": true,\n" + " \"name\": \"" + gui.getTextfield().getText() + "\"}";
+
+                    String subscribe = "{\"subscribe\": true }";
+                    socket.send(subscribe);
+                    byte[] subResponse = socket.recv();
+
+                    handleJSON(new String(subResponse, ZMQ.CHARSET));
+                    gui.setQueueArea(new String(subResponse, ZMQ.CHARSET));
+
+
+                    String payload = "{\"enterQueue\": true,\n" + " \"name\": \"" + gui.getNameTextField().getText() + "\"}";
+                    Person student = new Person("0", gui.getNameTextField().getText());
 
                     socket.send(payload);
 
                     byte[] response = socket.recv();
                     System.out.println("Reply from server: " + new String(response, ZMQ.CHARSET));
+
+
+                    JSONObject jsonObject = new JSONObject();
+
+                    //jsonObject.getJSONObject(new String(subResponse1, ZMQ.CHARSET));
 
 
                     while(true){
@@ -81,9 +96,33 @@ public class DSAssign1
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
         executorService.scheduleAtFixedRate(heartbeat, 0, 3, TimeUnit.SECONDS);
         }
-    public static void initGUI(){
+    public static void handleJSON(String str){
 
+        JSONObject jsonObject = new JSONObject(str);
+        JSONArray key = jsonObject.names();
+        key.iterator();
+        for(int i = 0 ; i < key.length(); i++){
+            if(key.getString(i) == "\"queue\""){
+                //JSONArray valuekey = key.getJSONArray(i);
+                JSONArray keyvalue = jsonObject.getJSONObject(key.getString(i)).names();
+                System.out.println("There was a queue message");
+                for(int j = 0; j < keyvalue.length(); j++){
+                    System.out.println("There was a key in the que");
+                }
+            }
 
+        }
+        /*
+        if(jsonObject.has("queue")) {
+            jsArr = jsonObject.getJSONArray("queue");
+            String qString = "";
+            for(int i =0; i<jsArr.length(); i++){
+                qString += (i + 1) + " : " + jsArr.getJSONObject(i).getString("name") + "\n";
+                //System.out.println("Names in queue" + jsArr.getJSONObject(i).getString("name") + "\n");
+
+            }
+           gui.getQueueArea(qString);
+        }*/
 
     }
 };
