85cae1b5b283d8a448e1345b4ceeeec5f67942e2
mhmdalkhyyt <mhmdalkhyyt@gmail.com>
2022-09-26 07:02:07 +0200

    Chaos changes

--- DSAssign1/src/main/java/com/dsassign1/ClientLogic.java
+++ DSAssign1/src/main/java/com/dsassign1/ClientLogic.java
@@ -5,18 +5,26 @@ import org.json.JSONObject;
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
 
+import java.util.ArrayList;
 
 
 public class ClientLogic {
     private String[] args;
+    private Prompt prompt;
     private GUI gui;
     private String queueStringList = "";
 
+    private ArrayList<String> students = new ArrayList<>();
+    private ArrayList<String> supervisors = new ArrayList<>();
 
-    public ClientLogic(String[] argStr, GUI window){
+
+
+    public ClientLogic(String[] argStr, GUI gui){
         this.args = argStr;
-        this.gui = window;
+        this.gui = gui;
+//        this.prompt = prompt;
 
     }
 
@@ -33,10 +41,18 @@ public class ClientLogic {
         ZMQ.Socket socket = context.createSocket(SocketType.DEALER);
         socket.connect(args[0]);
 
+        String subMsg = "{\"subscribe\": true\"}";
+        socket.send(subMsg);
+
+        byte[] subResp = socket.recv();
+        handleJSON(new String(subResp, ZMQ.CHARSET));
+
+
+
         while (gui.isEnterQueue()){
 
-                String subscribe = "{\"subscribe\": true }";
-                socket.send(subscribe);
+
+                heartbeat(socket);
 
 
                 String payload = "{\"enterQueue\": true,\n" + " \"name\": \"" + gui.getNameTextField().getText() + "\"}";
@@ -48,40 +64,64 @@ public class ClientLogic {
 
                 handleJSON(new String(response, ZMQ.CHARSET));
 
-                while(true){
+                /*while(true){
                     socket.send("{}");
                     Thread.sleep(4000);
                     System.out.println("4 secs passed, sending heartbeat");
-                    gui.setQueueArea("");
-                    socket.send(subscribe);
-                    byte[] subResponse = socket.recv();
-                    handleJSON(new String(subResponse, ZMQ.CHARSET));
+                    gui.getStudentList(students);
 
-                }
+                }*/
+
             }
     }
     public void handleJSON(String str){
         JSONObject jsonObject = new JSONObject(str);
-        JSONArray jsArr;
+        JSONArray studentjsArr;
+        JSONArray supervisorjsArr;
+
 
         if(jsonObject.has("queue")) {
-            jsArr = jsonObject.getJSONArray("queue");
+            studentjsArr = jsonObject.getJSONArray("queue");
 
-            System.out.println("Queue List is :" + jsArr);
-            for(int i =0; i < jsArr.length(); i++){
-                if(i <= jsArr.length() -1){
-                    queueStringList += (i+1) + " : " + jsArr.getJSONObject(i).getString("name") + "\n";
+            //System.out.println("Queue List is :" + jsArr);
+            for(int i =0; i < studentjsArr.length(); i++){
+                if(i <= studentjsArr.length() -1){
+                    queueStringList += (i+1) + " : " + studentjsArr.getJSONObject(i).getString("name") + "\n";
+                    students.add(studentjsArr.getJSONObject(i).getString("name"));
+
                 }
-                else if(i > jsArr.length()-1){
-                    queueStringList += (i++) + " : " + jsArr.getJSONObject(i +1).getString("name") + " <--- You";
+                else if(i > studentjsArr.length()-1){
+                    queueStringList += (i++) + " : " + studentjsArr.getJSONObject(i +1).getString("name") + " <--- You";
                 }
 
             }
 
+
             //queueStringList += jsArr.getJSONObject(jsArr.length() -1).getString("name") + " <--- YOU";
-            gui.setQueueArea(queueStringList);
+            gui.setQueueArea(students);
+        }
+        if(jsonObject.has("supervisors")){
+            supervisorjsArr = jsonObject.getJSONArray("supervisors");
+            for(int i= 0; i< supervisorjsArr.length(); i++){
+                supervisors.add(supervisorjsArr.getJSONObject(i).getString("name"));
+            }
+            gui.createSupervisorList(supervisors);
         }
 
+
     }
+
+    public ArrayList<String> getStudents(){
+        return students;
+    }
+
+    public void heartbeat(ZMQ.Socket socket) throws InterruptedException {
+
+        Thread.sleep(4000);
+        socket.send("{}");
+
+
+    }
+
 
 }
---
+++ DSAssign1/src/main/java/com/dsassign1/SupervisorView.java
@@ -0,0 +1,9 @@
+package com.dsassign1;
+
+public class SupervisorView extends GUI{
+
+    public SupervisorView(){
+
+    }
+
+}
